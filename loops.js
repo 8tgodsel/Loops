d562a4f00b1756381948b3fcc1f976cc542f5f3f
8tgodsel <31480893+8tgodsel@users.noreply.github.com>
2017-09-14 09:06:28 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,47 @@
+function loopOne(){
+
+    var counter = 30;
+    while (counter>0){
+if (counter% 5 == 0){
+console.log("AHHHHH" + counter + "seconds left!");
+}
+console.log(counter + "seconds left.");
+counter=counter-1;
+    }
+    console.log("Done Son!");
+
+}
+
+function loopTwo(){
+for(var i=0;i<n;i+2){
+
+console.log(i)}
+
+
+
+
+function loopThree(){
+
+
+}
+
+function main(){
+    //Testing each function
+    loopOne(10);
+    loopOne(25);
+
+    let powersOfTwo = loopTwo(2,10);
+    let powersOfFive = loopTwo(5,4);
+    console.log("The sum is "+powersOfTwo);
+    console.log("The sum is "+powersOfFive);
+
+    loopThree("#",6);
+    loopThree("$",20);
+    loopThree("x",10);
+
+    console.log("You will need to scroll up to check all of them.");
+}
+
+//Hit ctrl + Shift + B to run the program.
+//Hit ESC to make the printout messages go away.
+main();
